db826c6eebadd4c1ea42e5b67378ab0dc79e88ba
Bandi <bencsik.andras2003@gmail.com>
2024-06-21 22:56:36 +0200

    Container & RoundedRectangle Tests

--- tests/display/Container.tests.ts
+++ tests/display/Container.tests.ts
@@ -1,4 +1,5 @@
 import { Container } from '../../src/scene/container/Container';
+import { Matrix } from '../../src/maths/matrix/Matrix';
 
 describe('Container', () =>
 {
@@ -12,6 +13,117 @@ describe('Container', () =>
             expect(object.groupColor).toEqual(0xffffff);
             expect(object.renderable).toBe(true);
             expect(object.visible).toBe(true);
+        });
+
+        // Andras 1
+        it('should initialise height & width correctly', () =>
+        {
+            const container = new Container();
+
+            expect(container.width).toEqual(0);
+            expect(container.height).toEqual(0);
+
+            const initialWidth = container.width;
+            const initialHeight = container.height;
+
+            container.width = 100;
+            container.height = 200;
+
+            const expectedWidth = initialWidth * container.scale.x;
+            const expectedHeight = initialHeight * container.scale.y;
+
+            expect(container.width).toEqual(expectedWidth);
+            expect(container.height).toEqual(expectedHeight);
+        });
+
+        // Andras 2
+        it('should get and set correctly', () =>
+        {
+            const container = new Container();
+
+            let size = container.getSize();
+
+            expect(size.width).toEqual(0);
+            expect(size.height).toEqual(0);
+
+            const initialWidth = size.width;
+            const initialHeight = size.height;
+
+            container.setSize(100, 200);
+            size = container.getSize();
+
+            const expectedWidth = initialWidth * container.scale.x;
+            const expectedHeight = initialHeight * container.scale.y;
+
+            expect(size.width).toEqual(expectedWidth);
+            expect(size.height).toEqual(expectedHeight);
+        });
+
+        // Andras 3
+        it('should set pivot correctly when a number is passed', () =>
+        {
+            const container = new Container();
+
+            container.pivot = 100;
+
+            expect(container.pivot.x).toEqual(100);
+            expect(container.pivot.y).toEqual(100);
+        });
+
+        // Andras 4
+        it('should set pivot correctly when a point is passed', () =>
+        {
+            const container = new Container();
+
+            container.pivot = { x: 100, y: 200 };
+
+            expect(container.pivot.x).toEqual(100);
+            expect(container.pivot.y).toEqual(200);
+        });
+
+        // Andras 5
+        it('should copyFrom point to skew correctly', () =>
+        {
+            const container = new Container();
+            const point = { x: 3, y: 4 };
+
+            container.skew = point;
+
+            expect(container.skew.x).toEqual(3);
+            expect(container.skew.y).toEqual(4);
+        });
+    });
+
+    // Test Suite - Andras
+    describe('setFromMatrix', () =>
+    {
+        // Andras 6
+        it('should decompose the matrix into the container', () => {
+            const container = new Container();
+            const matrix = new Matrix();
+
+            // Set some values on the matrix
+            matrix.a = 1;
+            matrix.b = 2;
+            matrix.c = 3;
+            matrix.d = 4;
+            matrix.tx = 5;
+            matrix.ty = 6;
+
+            container.setFromMatrix(matrix);
+
+            const skewX = -Math.atan2(-matrix.c, matrix.d);
+            const skewY = Math.atan2(matrix.b, matrix.a);
+
+            const delta = Math.abs(skewX + skewY);
+            const PI_2 = Math.PI / 2;
+            const expectedRotation = (delta < 0.00001 || Math.abs(PI_2 - delta) < 0.00001) ? skewY : 0;
+
+            expect(container.position.x).toEqual(matrix.tx);
+            expect(container.position.y).toEqual(matrix.ty);
+            expect(container.scale.x).toEqual(Math.sqrt(matrix.a * matrix.a + matrix.b * matrix.b));
+            expect(container.scale.y).toEqual(Math.sqrt(matrix.c * matrix.c + matrix.d * matrix.d));
+            expect(container.rotation).toEqual(expectedRotation);
         });
     });
 
