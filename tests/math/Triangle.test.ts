e5961980372f99a913f7c53c3ae45d5b4f664953
bhalacsy <balinthalacsy@gmail.com>
2024-06-24 13:14:01 +0200

    defaultValue and Triangle test, package.json updated

---
+++ tests/math/Triangle.test.ts
@@ -0,0 +1,84 @@
+import { Rectangle } from '../../src/maths/shapes/Rectangle';
+import { Triangle } from '../../src/maths/shapes/Triangle';
+
+describe('Triangle', () =>
+{
+    it('returning correct values for default triangle', () =>
+    {
+        const triangle = new Triangle();
+
+        expect(triangle.x).toBe(0);
+        expect(triangle.y).toBe(0);
+        expect(triangle.x2).toBe(0);
+        expect(triangle.y2).toBe(0);
+        expect(triangle.x3).toBe(0);
+        expect(triangle.y3).toBe(0);
+    });
+
+    it('return triangle with specific values', () =>
+    {
+        const triangle = new Triangle(12, 13, 4, 3, 5, 13);
+
+        expect(triangle.x).toBe(12);
+        expect(triangle.y).toBe(13);
+        expect(triangle.x2).toBe(4);
+        expect(triangle.y2).toBe(3);
+        expect(triangle.x3).toBe(5);
+        expect(triangle.y3).toBe(13);
+    });
+
+    it('returning if triangle contains a given point', () =>
+    {
+        const triangle = new Triangle(0, 0, 10, 0, 5, 5);
+        const triangle2 = new Triangle(0, 0, 5, 0, 0, 5);
+
+        expect(triangle.contains(5, 2)).toBe(true);
+        expect(triangle.contains(15, 15)).toBe(false);
+        expect(triangle2.contains(2, -1)).toBe(false);
+    });
+
+    it('returning if triangle contains stroke', () =>
+    {
+        const triangle = new Triangle(0, 0, 10, 0, 5, 5);
+
+        expect(triangle.strokeContains(5, 0, 1)).toBe(true);
+        expect(triangle.strokeContains(5, 5, 1)).toBe(false);
+    });
+
+    it('returning clone of a given triangle', () =>
+    {
+        const triangle = new Triangle(12, 13, 4, 3, 5, 13);
+        const clone = triangle.clone();
+
+        expect(clone).toEqual(triangle);
+    });
+
+    it('should copy from another triangle correctly', () =>
+    {
+        const triangle1 = new Triangle(12, 13, 4, 3, 5, 13);
+        const triangle2 = new Triangle();
+
+        triangle2.copyFrom(triangle1);
+
+        expect(triangle2).toEqual(triangle1);
+    });
+
+    it('should copy to another triangle correctly', () =>
+    {
+        const triangle1 = new Triangle(12, 13, 4, 3, 5, 13);
+        const triangle2 = new Triangle();
+
+        triangle1.copyTo(triangle2);
+
+        expect(triangle2).toEqual(triangle1);
+    });
+
+    it('returns correct getBounds() triangle', () =>
+    {
+        const triangle = new Triangle(12, 13, 4, 3, 5, 13);
+        const bounds = triangle.getBounds();
+        const expectRectangle = new Rectangle(4, 3, 8, 10);
+
+        expect(bounds).toEqual(expectRectangle);
+    });
+});
---
+++ tests/renderering/defaultValue.test.ts
@@ -0,0 +1,65 @@
+import { defaultValue } from '../../src/rendering/renderers/gl/shader/program/defaultValue';
+
+describe('defaultValue', () =>
+{
+    it('return 0 for type "float"', () =>
+    {
+        expect(defaultValue('float', 1)).toBe(0);
+    });
+
+    it('returning correct sizes for Float32Arrays', () =>
+    {
+        expect(defaultValue('vec2', 1)).toEqual(new Float32Array(2));
+        expect(defaultValue('vec2', 2)).toEqual(new Float32Array(4));
+        expect(defaultValue('vec3', 1)).toEqual(new Float32Array(3));
+        expect(defaultValue('vec3', 2)).toEqual(new Float32Array(6));
+        expect(defaultValue('vec4', 1)).toEqual(new Float32Array(4));
+        expect(defaultValue('vec4', 2)).toEqual(new Float32Array(8));
+    });
+
+    it('return 0 for type "int" and "uint"', () =>
+    {
+        expect(defaultValue('int', 1)).toBe(0);
+        expect(defaultValue('uint', 1)).toBe(0);
+    });
+
+    it('returning correct sizes for Int32Arrays', () =>
+    {
+        expect(defaultValue('ivec2', 1)).toEqual(new Int32Array(2));
+        expect(defaultValue('ivec2', 2)).toEqual(new Int32Array(4));
+        expect(defaultValue('ivec3', 1)).toEqual(new Int32Array(3));
+        expect(defaultValue('ivec3', 2)).toEqual(new Int32Array(6));
+        expect(defaultValue('ivec4', 1)).toEqual(new Int32Array(4));
+        expect(defaultValue('ivec4', 2)).toEqual(new Int32Array(8));
+    });
+
+    it('returning correct sizes for type Uint32Arrays', () =>
+    {
+        expect(defaultValue('uvec2', 1)).toEqual(new Uint32Array(2));
+        expect(defaultValue('uvec2', 2)).toEqual(new Uint32Array(4));
+        expect(defaultValue('uvec3', 1)).toEqual(new Uint32Array(3));
+        expect(defaultValue('uvec3', 2)).toEqual(new Uint32Array(6));
+        expect(defaultValue('uvec4', 1)).toEqual(new Uint32Array(4));
+        expect(defaultValue('uvec4', 2)).toEqual(new Uint32Array(8));
+    });
+
+    it('return false for type "bool" and boolean arrays', () =>
+    {
+        expect(defaultValue('bool', 1)).toBe(false);
+        expect(defaultValue('bvec2', 1)).toEqual([false, false]);
+        expect(defaultValue('bvec3', 1)).toEqual([false, false, false]);
+        expect(defaultValue('bvec4', 1)).toEqual([false, false, false, false]);
+    });
+
+    it('return Float32Arrays', () =>
+    {
+        expect(defaultValue('mat2', 1)).toEqual(new Float32Array([1, 0, 0, 1]));
+        expect(defaultValue('mat3', 1)).toEqual(new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1]));
+        expect(defaultValue('mat4', 1)).toEqual(new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]));
+    });
+
+    it('return null for unknown type', () =>
+    {
+        expect(defaultValue('unknown', 1)).toBeNull();
+    });
+});
